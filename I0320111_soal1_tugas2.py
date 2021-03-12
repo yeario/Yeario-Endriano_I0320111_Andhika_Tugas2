9e22576a7df2761f1056afb76dfe0b5fe866c493
Yeario Endriano <endrianoyeario1@gmail.com>
2021-03-13 01:05:17 +0700

    Commit and Push

--- I0320111_soal1_tugas2.py
+++ I0320111_soal1_tugas2.py
@@ -1,1 +1,32 @@
+#menghitung luas persegi panjang
+panjang = 10
+lebar = 20
+luas_persegipanjang = panjang * lebar
+print("panjang: ", panjang)
+print("lebar: ", lebar)
+print("luas persegi panjang: ", luas_persegipanjang)
+
+#menghitung luas lingkaran
+r = 20
+luas_lingkaran = 3.14 * (r ** 2)
+print("r: ", r)
+print("luas lingkaran: ", luas_lingkaran)
+
+#menghitung luas kubus
+s = 20
+luas_permukaan_kubus = 6 * (s ** 2)
+print("s: ", s)
+print("luas permukaan kubus: ", luas_permukaan_kubus)
+
+#menghitung koversi suhu celcius ke fahrenheit
+C = 20
+F = ((9/5) * C) + 32
+print("C: ", C)
+print("F: ", F)
+
+#menghitung suhu reamur ke kelvin
+R = 20
+K = ((5/4) * R) + 273
+print("R: ", R)
+print("K: ", K)
 
--- I0320111_soal2_tugas2.py
+++ I0320111_soal2_tugas2.py
@@ -1,1 +1,37 @@
+# Identitas Mahasiswa
 
+nama = "Yeario Endriano"
+nama_panggilan = "Yeario"
+tanggal_lahir_tahun = "1 Januari 2002"
+pekerjaan = "Mahasiswa"
+jurusan = "Teknik Industri"
+universitas = "Universitas Negeri Sebelas Maret"
+tempat_lahir = "Magetan"
+anak_ke = 3
+alergi = "seafood"
+ukuran_sepatu = 42.7
+hobi = "koleksi sneakers"
+gol_darah = 'o'
+berat_badan = 52
+tinggi = 172
+alamat = "Kedungtungkul, RT2 RW7, Mojosongo, Jebres, Surakarta"
+cita_cita = "engineering manager"
+umur = 19
+instagram = "yearioendriano"
+olshop = "ogauction, our.dailygroup"
+komunitas_media = "evocative"
+organisasi = "Badan Eksekutif Mahasiswa Fakultas Teknik"
+
+print("Hai nama saya", nama, "umur saya", umur)
+print("Saya merupakan salah satu", pekerjaan,"dari jurusan", jurusan, universitas)
+print("Saya lahir pada", tanggal_lahir_tahun, "di", tempat_lahir)
+print("Setelah lulus kuliah, saya memiliki cita cita menjadi", cita_cita)
+print("Saya tinggal di", alamat)
+print("Saya merupakan anak ke", anak_ke, "dari", anak_ke, "bersaudara")
+print("Golongan darah saya adalah", gol_darah, "dan saya sudah berumur", umur, "tahun")
+print("Tinggi badan saya", tinggi, "cm", "dan berat badan saya adalah", berat_badan, "kg")
+print("Selama ini, saya memiliki hobi", hobi, "dan ukuran sepatu saya adalah", ukuran_sepatu)
+print("Saya juga memiliki alergi, terutama pada makanan", alergi)
+print("Dalam menjalankan hobi, saya juga mengikuti organisasi, yaitu", organisasi, "di", universitas)
+print("Di luar kampus saya juga mengikuti sebuah kelompok media yang bernama", komunitas_media)
+print("Terakhir saya miliki akun oline shop yaitu", olshop, "dan yang pasti jangan lupa follow instagram saya", instagram)
